d90608234c3def4e2839ea1fc9baea736701e60b
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:06:18 -0700

    Don't bother using the extension, eslint complains

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, prefer-promise-reject-errors */
 import {test} from "tap"
 
-import everyP from "./source.js"
+import endsWith from "./source"
 
 test(({same}) => {
   return everyP([
