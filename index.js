91bb9161a37a4fccc4f299ce012a9658afe9115e
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:29 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,36 +1,16 @@
-import mapValues from "@unction/mapvalues"
-import reduceValues from "@unction/reducevalues"
-import thenCatchP from "@unction/thencatchp"
-import allP from "@unction/allp"
-import thenP from "@unction/thenp"
-import couple from "@unction/couple"
+import mapValues from "@unction/mapvalues";
+import reduceValues from "@unction/reducevalues";
+import thenCatchP from "@unction/thencatchp";
+import allP from "@unction/allp";
+import thenP from "@unction/thenp";
+import couple from "@unction/couple";
+const initial = [[], []];
+export default function everyP(promises) {
+  return thenP(reduceValues(([resolved, rejected]) => ([state, value]) => {
+    if (state === "resolved") {
+      return couple([...resolved, value])(rejected);
+    }
 
-const initial = [[], []]
-
-export default function everyP (promises: Array<any | Promise<any>>): Promise<[ResolvedPromisesType, RejectedPromisesType]> {
-  return thenP(
-    reduceValues(
-      ([resolved, rejected]: [ResolvedPromisesType, RejectedPromisesType]): Function => ([state, value]: [string, any]): Function => {
-        if (state === "resolved") {
-          return couple([...resolved, value])(rejected)
-        }
-
-        return couple(resolved)([...rejected, value])
-      }
-    )(
-      initial
-    )
-  )(
-    allP(
-      mapValues(
-        thenCatchP(
-          couple("resolved")
-        )(
-          couple("rejected")
-        )
-      )(
-        promises
-      )
-    )
-  )
+    return couple(resolved)([...rejected, value]);
+  })(initial))(allP(mapValues(thenCatchP(couple("resolved"))(couple("rejected")))(promises)));
 }
